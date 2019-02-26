24a1c2d5eba9ad17bbf421886102a731aad2bb28
Domantas99 <domantas.greicius@gmail.com>
2019-02-26 19:23:09 +0200

    modulis

---
+++ Dofa.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: doman
+ * Date: 2/26/2019
+ * Time: 6:32 PM
+ */
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class Dofa extends Module
+{
+
+    public function __construct()
+    {
+        $this->name = 'Dofa';
+        $this->author = 'Domantas ir Faustas';
+        //$this->tab = 'front_office_features';
+        $this->version = '0.0.1';
+        //$this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Welcome to Dofa');
+        $this->description = $this->l(
+            'Sveitaine'
+        );
+        //$this->ps_versions_compliancy = array('min' => '1.7.2.0', 'max' => _PS_VERSION_);
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->registerHook('DisplayHome');
+    }
+
+    public function hookDisplayHome()
+    {
+        return '<h1>Sveiki atvyke!!</h1>';
+    }
+
+
+}
